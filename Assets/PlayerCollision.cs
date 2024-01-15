4fb41f3c224c5ec3cb8b5eb41b23226bf8ad70d6
Simona Kals <SimonaKals@users.noreply.github.com>
2024-01-15 18:22:12 +0100

    Collect Coins

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+  
+   
+void OnCollisionEnter (Collision collisionInfo){
+   if(collisionInfo.collider.tag=="Obstacle"){
+
+  Debug.Log("HIt");
+  Destroy(GameObject.FindWithTag("Obstacle"));
+
+   }
+}
+}
